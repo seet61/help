d78c1ad0fd931dcc5bf1181a98241b28a640850e
seet61 <seet61@gmail.com>
2017-01-23 20:35:18 +0300

    Исправлен шаблон добавления информации о работах Добавлена модель для работ

--- tasks/models.py
+++ tasks/models.py
@@ -5,10 +5,21 @@ from django.db import models
 class Tasks(models.Model):
 	number = models.CharField(max_length=10)
 	login = models.CharField(max_length=32)
-	activity_date = models.DateField()
+	activity_date = models.DateField(db_index=True)
 	comment = models.CharField(max_length=2048)
 
 class Over_Time(models.Model):
 	login = models.CharField(max_length=32)
-	activity_date = models.DateField()
+	activity_date = models.DateField(db_index=True)
+	comment = models.CharField(max_length=2048)
+
+class Works(models.Model):
+	number = models.CharField(max_length=10)
+	login = models.CharField(max_length=32)
+	activity_date = models.DateField(db_index=True)
+	field = models.BooleanField(default=False)
+	installed = models.CharField(max_length=4096)
+	restarted = models.CharField(max_length=4096)
+	logs = models.CharField(max_length=1024)
+	call_CC = models.BooleanField(default=False)
 	comment = models.CharField(max_length=2048)
