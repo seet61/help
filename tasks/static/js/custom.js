630001d6abf5bed67f55031a77c3e0a3e69010c6
seet61 <seet61@gmail.com>
2017-01-25 20:04:26 +0300

    1. Исправлен шаблон по пожеланиям коллег 2. Исправлен файл readme

--- tasks/static/js/custom.js
+++ tasks/static/js/custom.js
@@ -2,8 +2,15 @@ $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 10, // Creates a dropdown of 15 years to control year
     firstDay: true,
-    closeOnSelect: true,
-    closeOnClear: false
+    onSet: function () {
+   		this.close();
+	}
+});
+$('#timepicker').pickatime({
+    default: 'now',
+    autoclose: true,
+    twelvehour: false,
+    donetext: 'OK'
 });
 $(document).ready(function(){
     $('ul.tabs').tabs();
